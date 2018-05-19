e8c0c01a7771a6120b529c237188f07c86f25e28
speedcom <mateusz.maciaszekhpc@gmail.com>
2018-05-19 16:32:30 +0300

    send created question contest to all subscribers

--- server.js
+++ server.js
@@ -22,13 +22,27 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
-
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
+    console.log('Incoming data: ' + data)
+
+    const parsed = JSON.parse(data)
+    const question_duration_sec = parsed.question.duration
+    const payload = {
+      "partner_title" : parsed.partner_title,
+      "id"            : parsed.id,
+      "question"      : {
+        "title"   : parsed.question.title,
+        "options" : parsed.question.options.map(q => q.body)
+      }
+    }
+    const body = JSON.stringify(payload)
+    console.log('Payload: ' + body)
+
     // Broadcast to everyone else.
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
+        client.send(JSON.stringify(body));
       }
     });
   });
@@ -41,4 +55,3 @@ setInterval(() => {
     client.send(new Date().toTimeString());
   });
 }, 5000);
-
